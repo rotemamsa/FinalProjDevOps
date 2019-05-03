31cc75af189f72629b3b9537316742e264dcde59
Refael, Rotem <Rotem.Refael@verint.com>
2019-05-03 17:07:10 +0300

    New app for docker and selenium test

---
+++ dockerTest.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path='C:/GIT/devopsCourse/chromedriver_win32/chromedriver.exe')
+driver.implicitly_wait(10)
+driver.get("http://192.168.99.100:5000/")
+
+text = driver.find_element_by_class_name("pre")
+print(text.text)
+
