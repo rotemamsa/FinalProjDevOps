1d47cdc93b0b853e2af8d4123f8b8ea287734521
Refael, Rotem <Rotem.Refael@verint.com>
2019-05-04 14:34:18 +0300

    change x_path

--- dockerTest.py
+++ dockerTest.py
@@ -1,9 +1,9 @@
+import time
 from selenium import webdriver
 
 driver = webdriver.Chrome(executable_path='C:/GIT/devopsCourse/chromedriver_win32/chromedriver.exe')
 driver.implicitly_wait(10)
 driver.get("http://192.168.99.100:5000/")
 
-text = driver.find_element_by_class_name("pre")
+text = driver.find_element_by_xpath("//*")
 print(text.text)
-
